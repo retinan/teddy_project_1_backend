11c708c06b94161ae2d7dbcd38a15e4a6ff0ec77
retin <voicemin78@gmail.com>
2023-05-14 12:05:52 +0900

    forgot id 추가

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
-from api.views import CustomRegisterView, CustomLoginView
+from api.views import CustomRegisterView, CustomLoginView, FindUserID
 
 urlpatterns = [
     # 회원가입, 로그인 커스텀 오버라이딩
     # 순서가 중요 커스텀 url을 기본 제공 url 보다 먼저 위치해야 함.
     path('accounts/login/', CustomLoginView.as_view()),
     path('accounts/signup/', CustomRegisterView.as_view()),
+    path('accounts/forgot_id/', FindUserID.as_view()),
     # dj_rest_auth 회원가입, 로그인 기본 제공 URL
     path('accounts/', include('dj_rest_auth.urls')),
     path('accounts/signup/', include('dj_rest_auth.registration.urls')),
--- api/views.py
+++ api/views.py
@@ -2,8 +2,14 @@
 from rest_framework import status
 from rest_framework.response import Response
 from dj_rest_auth.registration.views import RegisterView
+from rest_framework.views import APIView
+
 from api.serializers.accountSerializer import CustomLoginSerializer, CustomRegisterSerializer
 from dj_rest_auth.views import LoginView
+
+import re
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
 
 
 class CustomLoginView(LoginView):
@@ -21,3 +27,26 @@ class CustomRegisterView(RegisterView):
     #     return super().create(request, *args, **kwargs)
 
 
+class FindUserID(APIView):
+    def post(self, request, format=None):
+        email = request.data.get('email')
+        if not email:
+            return Response({'message': '이메일을 입력해주세요.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            return Response({'message': '유효한 이메일 주소를 입력해주세요.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response({'message': '해당 이메일 주소를 가진 사용자가 존재하지 않습니다.'}, status=status.HTTP_404_NOT_FOUND)
+
+        send_mail(
+            'ID 찾기',
+            f'귀하의 ID는 {user.username}입니다.',
+            'noreply@example.com',
+            [email],
+            fail_silently=False,
+        )
+
+        return Response({'message': '입력하신 이메일 주소로 ID가 전송되었습니다.'}, status=status.HTTP_200_OK)
