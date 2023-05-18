466beec02d718338ec91de10a9b1b08ada26dc09
retin <voicemin78@gmail.com>
2023-05-18 19:50:04 +0900

    development DB pythonanywhere mysql db setting

--- backend/settings/development.py
+++ backend/settings/development.py
@@ -5,13 +5,24 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'akynmyot',
+#         'USER': 'akynmyot',
+#         'PASSWORD': '1RZnjual1iA42eFM8CVRKuwxpTz8hswM',
+#         'HOST': 'arjuna.db.elephantsql.com',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'akynmyot',
-        'USER': 'akynmyot',
-        'PASSWORD': '1RZnjual1iA42eFM8CVRKuwxpTz8hswM',
-        'HOST': 'arjuna.db.elephantsql.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'retinan$teddy_project_1',
+        'USER': 'retinan',
+        'PASSWORD': 'qwrt12@@',
+        'HOST': 'retinan.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
